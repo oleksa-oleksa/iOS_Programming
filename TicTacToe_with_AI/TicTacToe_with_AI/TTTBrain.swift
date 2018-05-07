76a8816bd9df008c4a09b59eecb2cd4005b5f883
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-05-07 19:20:33 +0200

    Two players logic done, design done. Starting with AI Task

--- TicTacToe_with_AI/TicTacToe_with_AI/TTTBrain.swift
+++ TicTacToe_with_AI/TicTacToe_with_AI/TTTBrain.swift
@@ -11,10 +11,17 @@ import UIKit
 
 class TTTBrain {
     
+    // for keeping the turns and preventing the double turn/click
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    var gameEnded:Bool = false
+    
+    /* Let is an immutable variable, meaning that it cannot be changed, other languages call this a constant. In C++ it you can define it as const. */
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                               [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                               [0, 4, 8], [2, 4, 6]]
 
     func isTurnAllowed(cellNo: Int) -> Bool {
-        if (gameState[cellNo]) == 0 {
+        if (gameState[cellNo]) == 0 && gameEnded != true {
             return true
         }
         else {
@@ -22,14 +29,31 @@ class TTTBrain {
         }
     }
     
-    func markCell(cellNo: Int) {
-        gameState[cellNo] = 1
+    func makeTurn(cellNo: Int, activePlayer: Int) -> Int {
+        // saving the player's turn
+        gameState[cellNo] = activePlayer
+        
+        var newActivePlayer:Int?
+        
+        if (activePlayer == 1) {
+            newActivePlayer = 2
+        }
+            
+        else if (activePlayer == 2) {
+            newActivePlayer = 1
+        }
+
+        return newActivePlayer!
     }
     
     func isGameOver() -> Bool {
+        // Player 1 has 5 turns marked with 1 ==> sum is 5 when player 1 has finished
+        // Player 2 has 4 turns marked with 2 ==> sum is 8 when player 2 has finished
+        // Total sum = 13 when gameis over
+        
         let sum = gameState.reduce(0, +)
         
-        if (sum > 8) {
+        if (sum > 12) {
             return true
         }
         else {
@@ -37,8 +61,26 @@ class TTTBrain {
         }
     }
     
+    func getWinner() -> (Int, [Int]) {
+        var winner:Int = 0
+        var winButtons:[Int] = [0, 0, 0]
+        
+        for combination in winningCombinations {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                
+                winner = gameState[combination[0]]
+                winButtons = combination
+                gameEnded = true
+            }
+        }
+        
+        return (winner, winButtons)
+    }
+    
+    
     func restartGame() {
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameEnded = false
     }
     
     func isNewGame() -> Bool {
--- TicTacToe_with_AI/TicTacToe_with_AI/ViewController.swift
+++ TicTacToe_with_AI/TicTacToe_with_AI/ViewController.swift
@@ -12,10 +12,25 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
+    // Game starts and Player 1 has current turn
+    // The settings for UILabel are set accordingly to Player 1
     var activePlayer:Int = 1
     var firstPlayer = 1 // Crosses
     var secondPlayer = 2 // Zeros
+    var winner:Int = 0
     
+    var activePlayerName:String = "Player 1"
+    var activePlayerSign = "cross.png"
+    var gameOverInfo:String = ""
+    var winButtons:[Int] = [0, 0, 0]
+
+    var color:UIColor?
+    var green:UIColor = UIColor(red: 113.0/255.0, green:
+        165.0/255.0, blue: 29.0/255.0, alpha: 1.0)
+    var blue:UIColor = UIColor(red: 20.0/255.0, green:
+        89.0/255.0, blue: 173.0/255.0, alpha: 1.0)
+    var red:UIColor = UIColor(red: 209.0/255.0, green:
+        35.0/255.0, blue: 35.0/255.0, alpha: 1.0)
     var ticTacToeGame:TTTBrain = TTTBrain()
     
     @IBOutlet weak var gameInfo: UILabel!
@@ -25,49 +40,83 @@ class ViewController: UIViewController {
     @IBAction func gameFieldButtonPressed(_ sender: AnyObject) {
         
         // preventing the double clicking on the game field
-        if (ticTacToeGame.isTurnAllowed(cellNo: sender.tag-1)) {
+        if ticTacToeGame.isTurnAllowed(cellNo: sender.tag-1) {
             
-            ticTacToeGame.markCell(cellNo: sender.tag - 1)
-            
-            // checking wheather the end of the game
-            if (ticTacToeGame.isGameOver()) {
-                gameInfo.text = "Game over!"
-                gameInfo.textColor = UIColor.red
-                playerInfo.text = ""
+            /****************************************/
+            // SWITCHING PLAYERS
+            /****************************************/
+            if activePlayer == 1 {
+                activePlayerSign = "cross.png"
+                activePlayerName = "Player 2"
+                color = blue
             }
-            else {
-                gameInfo.text = ""
+                
+            else if activePlayer == 2 {
+                activePlayerSign = "zero.png"
+                activePlayerName = "Your Turn"
+                color = green
             }
             
             /****************************************/
+            // SAVING THE TURNS
+            /****************************************/
+            activePlayer = ticTacToeGame.makeTurn(cellNo: sender.tag - 1, activePlayer: activePlayer)
             
-            if (activePlayer == 1) {
-                sender.setImage(UIImage(named: "cross.png"), for: UIControlState())
-                playerStatusInfo.text = "Player 2"
-                playerStatusInfo.textColor = UIColor.blue
-                activePlayer = secondPlayer
+            /****************************************/
+            // GAME OVER: NO WINNER
+            /****************************************/
+            if (ticTacToeGame.isGameOver()) {
+                activePlayerName = "That's all folks!"
                 
-                if (ticTacToeGame.isGameOver()) {
-                    playerStatusInfo.text = "That's all folks!"
-                    playerStatusInfo.textColor = UIColor.darkGray
-                }
-
+                gameOverInfo = "Game Over!"
+                color = red
             }
-            else if (activePlayer == 2) {
-                sender.setImage(UIImage(named: "zero.png"), for: UIControlState())
-                playerStatusInfo.text = "Player 1"
-                playerStatusInfo.textColor = UIColor.green
-                activePlayer = firstPlayer
-                
-                if (ticTacToeGame.isGameOver()) {
-                    playerStatusInfo.text = "That's all folks!"
-                    playerStatusInfo.textColor = UIColor.darkGray
+            
+            /****************************************/
+            // GAME OVER: SOMEONE WON
+            /****************************************/
+            (winner, winButtons) = ticTacToeGame.getWinner()
+            
+            if winner == 1 {
+                activePlayerName = "YOU WON!"
+                gameOverInfo = "Game Over!"
+                color = UIColor(red: 113.0/255.0, green:
+                    165.0/255.0, blue: 29.0/255.0, alpha: 1.0)
+            }
+            
+            if winner == 2 {
+                activePlayerName = "COMPUTER WON!"
+                gameOverInfo = "Game Over!"
+                color = UIColor(red: 209.0/255.0, green:
+                    35.0/255.0, blue: 35.0/255.0, alpha: 1.0)
+            }
+            
+            /****************************************/
+            // DESIGN
+            /****************************************/
+            sender.setImage(UIImage(named: activePlayerSign), for: UIControlState())
+            playerStatusInfo.text = activePlayerName
+            playerStatusInfo.textColor = color
+            gameInfo.text = gameOverInfo
+            gameInfo.textColor = red
+            
+            // Designing winning buttons
+            let sum = winButtons.reduce(0, +)
+            // if winner conbination was set
+            if  sum != 0 {
+                for i in winButtons {
+                    if let winButton = self.view.viewWithTag(i + 1) as? UIButton  {
+                        
+                        if winner == 1 {
+                            winButton.setImage(UIImage(named: "cross_won.png"), for: UIControlState())
+                        }
+                        
+                        if winner == 2 {
+                            winButton.setImage(UIImage(named: "zeros_won.png"), for: UIControlState())
+                        }
+                    }
                 }
             }
-        }
-        
-        else {
-            ticTacToeGame.markCell(cellNo: sender.tag - 1)
         }
     }
     
@@ -75,6 +124,7 @@ class ViewController: UIViewController {
     @IBAction func restartButtonPressed(_ sender: Any) {
         ticTacToeGame.restartGame()
         greetPlayers()
+        activePlayer = firstPlayer;
         
         for i in 1...9 {
             if let tmpButton = self.view.viewWithTag(i) as? UIButton {
@@ -88,7 +138,8 @@ class ViewController: UIViewController {
         gameInfo.textColor = UIColor.blue
         playerInfo.text = "Current turn:"
         playerStatusInfo.text = "Player 1"
-        playerStatusInfo.textColor = UIColor.green
+        playerStatusInfo.textColor = UIColor(red: 113.0/255.0, green:
+            165.0/255.0, blue: 29.0/255.0, alpha: 1.0)
     }
     
     override func viewDidLoad() {
@@ -105,4 +156,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
+
+
 
