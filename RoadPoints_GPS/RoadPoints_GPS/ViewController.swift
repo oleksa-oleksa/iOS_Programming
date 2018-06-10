5acc4a878152225bba1a39365649bf9287443aa7
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-06-11 01:06:15 +0200

    GPS with Berlin Coordinates simulated. Todo: Label Design and unset the label's text

--- RoadPoints_GPS/RoadPoints_GPS/ViewController.swift
+++ RoadPoints_GPS/RoadPoints_GPS/ViewController.swift
@@ -12,6 +12,53 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager:CLLocationManager = CLLocationManager()
+    var color:UIColor?
+    var green:UIColor = UIColor(red: 113.0/255.0, green:
+        165.0/255.0, blue: 29.0/255.0, alpha: 1.0)
+    
+    // Labels for graphic stations
+    @IBOutlet weak var station01: UILabel!
+    @IBOutlet weak var station02: UILabel!
+    @IBOutlet weak var station03: UILabel!
+    @IBOutlet weak var station04: UILabel!
+    @IBOutlet weak var station05: UILabel!
+    @IBOutlet weak var station06: UILabel!
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for currenLocation in locations {
+            print("\(index): \(currenLocation)")
+            let foundIdx = compareLocations(location: currenLocation)
+            if foundIdx != 0 {
+                activateLocation(index: foundIdx)
+            }
+        }
+    }
+    
+    func activateLocation(index: Int) {
+        switch index {
+        case 1:
+            station01.text = stationsTask[0]
+        case 2:
+            station02.text = stationsTask[1]
+        case 3:
+            station03.text = stationsTask[2]
+        case 4:
+            station04.text = stationsTask[3]
+        case 5:
+            station05.text = stationsTask[4]
+        case 6:
+            station06.text = stationsTask[5]
+        default:
+            station01.text = "Station 1"
+            station02.text = "Station 2"
+            station03.text = "Station 3"
+            station04.text = "Station 4"
+            station05.text = "Station 5"
+            station06.text = "Station 6"
+
+        }
+        
+    }
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +67,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        locationManager.distanceFilter = 100
+        locationManager.distanceFilter = 10
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        for currenLocation in locations {
-            print("\(index): \(currenLocation)")
-        }
-    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- RoadPoints_GPS/RoadPoints_GPS/stationStructure.swift
+++ RoadPoints_GPS/RoadPoints_GPS/stationStructure.swift
@@ -17,13 +17,34 @@ struct Station {
 
 let radiusWithBike = 250.0 // meters
 
-// Array for keepeng all 6 stations
-
+// Array for all 6 stations
 let stationsOnTheRoad = [station01, station02, station03,
                         station04, station05, station06]
+// Array for all 6 tasks
+let stationsTask = ["Buy a pinta of a craft beer",
+                    "Borrow the new engineering magazine \"Make\" from Uni Library",
+                    "Buy a food for a kitten",
+                    "Do some sport",
+                    "Buy a new Sudoku for a beloved wife",
+                    "Visit the Monkey bar and drink a fresh coffee"]
+
+var foundLocation = [0, 0, 0, 0, 0, 0]
+
+func compareLocations(location: CLLocation) -> Int {
+    for (idx, thisLocation) in stationsOnTheRoad.enumerated() {
+        let distance = haversineDinstance(la1: thisLocation.lat, lo1: thisLocation.lon, la2: location.coordinate.latitude, lo2: location.coordinate.longitude)
+        if distance < radiusWithBike {
+            foundLocation[idx] = 1
+            return idx + 1 // the index of a corresponding station
+        }
+        else {
+            foundLocation[idx] = 0
+        }
+    }
+    return 0 // no index found
+}
 
 // STATIONS
-
 // Vagabung Brewery
 // Task: Buy a pinta of a craft beer
 let station01 = Station(lat: 52.546254,
@@ -31,13 +52,13 @@ let station01 = Station(lat: 52.546254,
                         radius: radiusWithBike)
 
 // Technical University Berlin
-// Task: Borrow the new exemplar of engineering magazine "Make"
+// Task: Borrow the new engineering magazine "Make" from Uni Library
 let station02 = Station(lat: 52.510038,
                         lon: 13.338477,
                         radius: radiusWithBike)
 
 // Petshop
-// Task: Buy a food fot a kitten
+// Task: Buy a food for a kitten
 let station03 = Station(lat: 52.491482,
                         lon: 13.297582,
                         radius: radiusWithBike)
@@ -48,7 +69,7 @@ let station04 = Station(lat: 52.458683,
                         radius: radiusWithBike)
 
 // U-Bahn Kurfürstendamm
-// Task: Buy a new Sudoku fot a beloved wife
+// Task: Buy a new Sudoku for a beloved wife
 let station05 = Station(lat: 52.503159,
                         lon: 13.331907,
                         radius: radiusWithBike)
@@ -57,3 +78,6 @@ let station05 = Station(lat: 52.503159,
 let station06 = Station(lat: 52.507323,
                         lon: 13.336106,
                         radius: radiusWithBike)
+
+
+
