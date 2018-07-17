4b93f69959aba3bccd40a50a53e6a6200d689046
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-07-17 22:09:45 +0200

    Exam: Find error

--- examTraining/02_findError.playground/Contents.swift
+++ examTraining/02_findError.playground/Contents.swift
@@ -1,7 +1,7 @@
 class MyClass {
     var a  = 20
-    
-    init (aWert: int) {
+
+    init (aWert: Int) {
         a = 10
     }
 }
@@ -22,18 +22,19 @@ if i != nil {
 //class MyClass {
 //    var a  = 20
 //
-//    init (aWert: int) {
+//    init (aWert: int) { // Int capital letter
 //        a = 10
 //    }
 //}
 //
 //let myInstance = MyClass(aWert: 10)
-//let anotherMyInstance = MyClass() // error: cannot invoke initializer for type 'MyClass' with no arguments
+//let anotherMyInstance = MyClass() // error: error: missing argument for parameter 'aWert' in call
+//
 //
 //let myStr = "Highlander"
 //myStr += " and another Highlander" // error: left side of mutating operator isn't mutable: 'myStr' is a 'let' constant
 //
-//let i = 1
+//let i = 1 // note: change 'let' to 'var' to make it mutable
 //
 //if i { // error: 'Int' is not convertible to 'Bool'
 //    print("Alles ok")
